47e41b392f65e7ef7bf7a586054697360536b70f
thehellbean <me@hellbean.com>
2020-07-13 10:33:55 +0200

    Initialize with Spring initializer

---
+++ src/main/java/com/hellbean/bbbs/BbbsApplication.java
@@ -0,0 +1,13 @@
+package com.hellbean.bbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellbean/bbbs/BbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellbean.bbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

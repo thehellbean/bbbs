2935c084d8ceb1cf35a34d822ea0c4bae6f84231
thehellbean <me@hellbean.com>
2020-07-15 08:55:41 +0200

    Implement remaining HTTP methods for PostController

--- src/main/java/com/hellbean/bbbs/PostController.java
+++ src/main/java/com/hellbean/bbbs/PostController.java
@@ -2,8 +2,10 @@ package com.hellbean.bbbs;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,5 +31,22 @@ class PostController {
     @GetMapping("/posts/{id}")
     Post one(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
+    }
+
+    @DeleteMapping("/posts/{id}")
+    void deletePost(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @PutMapping("/posts/{id}")
+    Post replacePost(@RequestBody Post newPost, @PathVariable Long id) {
+        return repository.findById(id).map(post -> { 
+            post.setTitle(newPost.getTitle());
+            post.setContent(newPost.getContent()); 
+            return repository.save(post); 
+        }).orElseGet(() -> {
+            newPost.setId(id);
+            return repository.save(newPost);
+        });
     }
 }
